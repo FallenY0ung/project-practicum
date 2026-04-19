32d5d4dfec5b54e1b855dc0b93e93f273d399a87
FallenY0ung <Sasha-kotlov.sasha@yandex.ru>
2026-04-19 21:37:25 +0400

    fix: spotless apply

--- src/test/java/ru/tbank/practicum/kafka/KafkaCommandProducerTest.java
+++ src/test/java/ru/tbank/practicum/kafka/KafkaCommandProducerTest.java
@@ -7,10 +7,10 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.core.KafkaTemplate;
 import ru.tbank.practicum.kafka.dto.BlindsCommandMessage;
 import ru.tbank.practicum.kafka.dto.RadiatorCommandMessage;
 
